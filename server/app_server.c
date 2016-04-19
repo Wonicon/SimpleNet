b1f03a79a257126ae034a01b59792ed9525a9b63
Wonicon <conciselove@outlook.com>
2016-04-20 00:11:00 +0800

    Finish son_start()

--- server/app_server.c
+++ server/app_server.c
@@ -10,6 +10,7 @@
 
 //输出: STCP服务器状态
 
+#include "common.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -20,6 +21,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <limits.h>
 
 #include "../common/constants.h"
 #include "stcp_server.h"
@@ -33,25 +35,78 @@
 #define WAITTIME 10
 
 //这个函数通过在客户和服务器之间创建TCP连接来启动重叠网络层. 它返回TCP套接字描述符, STCP将使用该描述符发送段. 如果TCP连接失败, 返回-1.
+
+static short son_port = 0;
+
+/**
+ * @brief 创建与客户的 TCP 连接作为底层网络
+ * @return 与客户端的连接套接字
+ *
+ * son_start() 将像常规服务器一样，创建套接字，监听端口，等待客户连接。
+ * 但是需要使用的只有连接套接字，这个 TCP 连接将作为分组交换网络（虽然是可靠交付的……）。
+ *
+ * 至于如何在 STCP 层创建多个连接，目前只能在客户端进程中多次建立连接实现。
+ */ 
 int son_start()
 {
+    // 创建套接字
+    int fd = socket(AF_INET, SOCK_STREAM, 0);  // 监听套接字，IP 地址族，流数据（TCP）
+    if (fd < 0) {
+        sys_panic("socket");
+    }
+
+    // 将套接字绑定到本机 IP 地址 + 端口号。
+    struct sockaddr_in serv_addr = {
+        .sin_family      = AF_INET,
+        .sin_addr.s_addr = INADDR_ANY,
+        .sin_port        = htons(son_port),
+    };
+
+    if (bind(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) {
+        sys_panic("bind");
+    }
+
+    // 监听连接。
+    listen(fd, 5);
+
+    // 只需要一个连接。
+    struct sockaddr_in inaddr;  // useless
+    socklen_t inaddr_len;       // useless
+    return accept(fd, (struct sockaddr *)&inaddr, &inaddr_len);
 }
 
 //这个函数通过关闭客户和服务器之间的TCP连接来停止重叠网络层
 void son_stop(int son_conn)
 {
+    close(son_conn);
+    // 在进程退出后自动关闭监听套接字。
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     //用于丢包率的随机数种子
     srand(time(NULL));
+
+    if (argc < 2) {
+        son_port = 4000;
+    }
+    else {
+        int port = atoi(argv[1]);
+        if (!(port > 0 && port < SHRT_MAX)) {
+            panic("%d exceeds short limit", port);
+        }
+        else {
+            son_port = (short)port;
+        }
+    }
 
     //启动重叠网络层并获取重叠网络层TCP套接字描述符
     int son_conn = son_start();
     if(son_conn < 0) {
         printf("can not start overlay network\n");
     }
+
+    log("son started!");
 
     //初始化STCP服务器
     stcp_server_init(son_conn);
