b6b849f04ba0a50f5952a04885c49ff40d81c9c4
Wonicon <conciselove@outlook.com>
2016-04-29 15:28:22 +0800

    Overwrite previous results

--- server/app_stress_server.c
+++ server/app_stress_server.c
@@ -116,7 +116,7 @@ int main(int argc, char *args[]) {
 
 	//将接收到的文件数据保存到文件receivedtext.txt中
 	FILE* f;
-	f = fopen("receivedtext.txt","a");
+	f = fopen("receivedtext.txt","w");
 	fwrite(buf,fileLen,1,f);
 	fclose(f);
 	free(buf);
