d07ae0a09b1d7443afbcdaa8e7ed04427c991250
Wonicon <conciselove@outlook.com>
2016-05-06 01:23:42 +0800

    删除不需要的 SON_PORT 定义
    由于使用 Unix Domain 进行进程间通信，不需要再占用端口。
    SON_PORT 不再使用，故删去。如果使用 TCP 进行通信，需要修改
    频繁修改宏定义避免和别人冲突，更为麻烦。

--- common/constants.h
+++ common/constants.h
@@ -14,9 +14,6 @@
 //这个端口号用于重叠网络中节点之间的互联, 你应该修改它为一个随机值以避免和其他同学的设置发生冲突
 #define CONNECTION_PORT 3000
 
-//这个端口号由SON进程打开, 并由SIP进程连接, 你应该修改它为一个随机值以避免和其他同学的设置发生冲突
-#define SON_PORT 3500
-
 //最大SIP报文数据长度: 1500 - sizeof(sip header)
 #define MAX_PKT_LEN 1488
 
